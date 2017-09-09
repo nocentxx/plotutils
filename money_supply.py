fb15ce7dad0dfb68de95a11d7ddb3f1bc7727fe0
littleminer <xuxin0509@gmail.com>
2017-09-09 18:46:22 +0800

    [refactor] use string variable instead of hard code to set plot property

--- money_supply.py
+++ money_supply.py
@@ -4,22 +4,27 @@ import matplotlib.pyplot as pyplot
 import numpy
 import pandas
 
+x_label = 'Time(s)'
+y_label = 'M2'
+plot_title = 'Money Supply Unit:100 Million Yuan'
+ms_png_name = 'money_supply.png'
+
 time_money = pandas.read_csv('money_supply.csv')
 
 t = time_money.T.values[0]
 m = time_money.T.values[1]
 x = numpy.arange(1, len(t) + 1)
 
-pyplot.plot(x, m)
+pyplot.plot(x, m, 'ro-')
 
-pyplot.xlabel('Time(s)')
+pyplot.xlabel(x_label)
 pyplot.xticks(x, t.tolist(), rotation=45)
 
-pyplot.ylabel('M2')
+pyplot.ylabel(y_label)
 
-pyplot.title('Money Supply Unit:100 Million Yuan')
+pyplot.title(plot_title)
 
 pyplot.grid(True)
 
-pyplot.savefig("Money_Supply.png")
+pyplot.savefig(ms_png_name)
 pyplot.show()
